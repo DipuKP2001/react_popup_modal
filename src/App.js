285778adafb314324642d393b90eb2a1a3a20701
DipuKP2001 <dipukp602@gmail.com>
2020-10-31 13:58:18 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Modal from './Modal'
 
-function App() {
-  return (
-    <>
-    <div>
-      <button>Open Modal</button>
-    </div>
-    <div>Other Content</div>
-    </>
-  );
+const BUTTON_WRAPPER_STYLES = {
+  position: 'relative',
+  zIndex: 1
 }
 
-export default App;
+const OTHER_CONTENT_STYLES = {
+  position: 'relative',
+  zIndex: 2,
+  backgroundColor: 'red',
+  padding: '10px'
+}
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <>
+      <div style={BUTTON_WRAPPER_STYLES} onClick={() => console.log('clicked')}>
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+          Fancy Modal
+        </Modal>
+      </div>
+
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+    </>
+  )
+}
